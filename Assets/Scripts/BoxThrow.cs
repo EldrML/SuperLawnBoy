158342fa1f5d9775b74c2ebe1a7125c1c2e03440
Liam Durbin <ldompro@gmail.com>
2020-10-28 22:10:51 -0400

    set up a signaling system using ScriptableObjects, this will be used for health, amount of grass in a room, etc.

---
+++ Assets/Scripts/BoxThrow.cs
@@ -0,0 +1,42 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class BoxThrow : Box
+ {
+    //  Vector3 startPos = new Vector3(0, 0, 0);
+    //  Vector3 endPos = new Vector3(0, 0, 10);
+    //  float height = 4f;
+    //  bool startThrow = false;
+    //  float incrementor = 0;
+ 
+     // Update is called once per frame
+    //  void Update()
+    //  {
+    //      startPos   = player.transform.position + carryHeight;
+    //      endPos     = throwLocation;
+
+    //      if (startThrow)
+    //      {
+    //          incrementor += 0.04f;
+    //          Vector3 currentPos = Vector3.Lerp(startPos, endPos, incrementor);
+    //          currentPos.z += height * Mathf.Sin(Mathf.Clamp01(incrementor) * Mathf.PI);
+    //          transform.position = currentPos;
+    //      }
+    //      if (transform.position == endPos)
+    //      {
+    //          startThrow = false;
+    //          incrementor = 0;
+    //          Vector3 tempPos = startPos;
+    //          startPos = transform.position;
+    //          endPos = tempPos;
+
+    //         boxState = BoxStates.onGround;
+    //         player.currentType = PlayerController.PlayerType.nm;
+    //         sprite.sortingLayerName = "Interactive";
+    //      }
+    //      if (boxState == BoxStates.isThrown)
+    //      {
+    //          startThrow = true;
+    //      }
+    //  }
+ }
---
+++ Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu] //Lets you create the object via a right click in Unity.
+public class FloatValue : ScriptableObject
+{
+    public float initialValue;
+}
